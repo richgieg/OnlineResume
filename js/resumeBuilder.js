9cef2074b253c498f9a7b8a6dff8c65ad00365ad
Richard Gieg <richgieg@users.noreply.github.com>
2015-09-14 10:02:29 -0700

    Complete lesson 1 quiz 2

---
+++ js/resumeBuilder.js
@@ -0,0 +1,6 @@
+// $("#main").append("Richard Gieg");
+var awesomeThoughts = "I am Richard and I am AWESOME!";
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
