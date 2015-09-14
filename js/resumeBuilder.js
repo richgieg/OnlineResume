1047a19012a180b30c27c2351c58b7fe2ed86f47
Richard Gieg <richgieg@users.noreply.github.com>
2015-09-14 12:51:18 -0700

    Complete lesson 2 quiz 5

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -91,19 +91,25 @@ var education = {
 //     var
 // }
 
-for (job in work.jobs) {
-    $("#workExperience").append(HTMLworkStart);
+function displayWork() {
+    for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
 
-    var formattedEmployer =
-        HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;
-    $(".work-entry:last").append(formattedEmployerTitle);
+        var formattedEmployer =
+            HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle =
+            HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
 
-    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    $(".work-entry:last").append(formattedDates);
+        var formattedDates =
+            HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
 
-    var formattedDescription =
-        HTMLworkDescription.replace("%data%", work.jobs[job].description);
-    $(".work-entry:last").append(formattedDescription);
+        var formattedDescription =
+            HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
 }
+
+displayWork();
