bc7a7ea170e86663c3287c749dc8bb293d2eb171
Richard Gieg <richgieg@users.noreply.github.com>
2015-09-14 11:02:09 -0700

    Complete lesson 1 quiz 10

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,36 @@
-// $("#main").append("Richard Gieg");
-var awesomeThoughts = "I am Richard and I am AWESOME!";
-console.log(awesomeThoughts);
+var bio = {
+    "name": "Richard Gieg",
+    "role": "Software Engineer",
+    "contacts": {
+        "mobile": "555-555-5555",
+        "email": "johndoe@example.com",
+        "github": "johndoe",
+        "twitter": "@johndoe",
+        "location": "SF Bay Area"
+    },
+    "welcomeMessage": "lorm ipsum dolor sit amet etc etc etc.",
+    "skills": [
+        "awesomeness", "delivering things", "cryogenic sleep", "saving the world"
+    ],
+    "bioPic": "images/fry.jpg"
+};
 
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
+ var formattedName = HTMLheaderName.replace("%data%", bio.name);
+ var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+var work = {};
+work.position = "Course Developer";
+work.employer = "Udacity";
+work.years = 0.3;
+
+var education = {};
+education["name"] = "Nova Southeastern University";
+education["years"] = "2005-2013";
+education["city"] = "Fort Lauderdale, FL, US";
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
