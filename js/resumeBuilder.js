050f16553409e67cc98fb11409457ffa711f364a
Richard Gieg <richgieg@users.noreply.github.com>
2015-09-14 12:45:07 -0700

    Complete lesson 2 quiz 4

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -98,6 +98,12 @@ for (job in work.jobs) {
         HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
     var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
     var formattedEmployerTitle = formattedEmployer + formattedTitle;
-
     $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+
+    var formattedDescription =
+        HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
 }
